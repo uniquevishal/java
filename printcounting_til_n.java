cf1bc8c374ee5c06618de736b999231ba1a05d24
uniquevishal <96346641+uniquevishal@users.noreply.github.com>
2022-01-19 10:30:59 +0530

    Create printcounting_til_n.java

---
+++ printcounting_til_n.java
@@ -0,0 +1,20 @@
+/* print counting from 1 to up to 5 */
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  int i=1;
+  while(i<=5){
+  
+  System.out.println(i);
+  i++;
+
+  }
+  System.out.println("done");
+  
+
+
+  }
+}
