5d6c6436a38fc4fc67623c2c889776dfecb1db8d
uniquevishal <96346641+uniquevishal@users.noreply.github.com>
2022-01-22 20:23:33 +0530

    Create benjamin.java

---
+++ benjamin.java
@@ -0,0 +1,15 @@
+/* https://www.pepcoding.com/resources/online-java-foundation/getting-started/benjamin-bulbs-official/ojquestion */
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    int n=scn.nextInt();
+    
+    for(int i=1;i*i<=n;i++){
+        System.out.println(i*i);
+    }
+    
+   }
+  }
