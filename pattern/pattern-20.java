ec7312b2a25161675b3837a7a806802e53e72626
uniquevishal <96346641+uniquevishal@users.noreply.github.com>
2022-01-31 16:36:57 +0530

    Create pattern-20.java

---
+++ pattern/pattern-20.java
@@ -0,0 +1,32 @@
+/* https://nados.io/question/pattern-20 */
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n=scn.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i<=n/2){
+                    if(j==1 || j==n){
+                         System.out.print("*\t");   
+                    }else{
+                        System.out.print("\t");
+                    }
+                }else{
+                    if(j==1 || j==n || i==j || i+j==n+1){
+                        System.out.print("*\t");
+                    }else{
+                         System.out.print("\t");
+                    }
+                }
+               
+            }
+            System.out.println();
+        }
+
+    }
+}
